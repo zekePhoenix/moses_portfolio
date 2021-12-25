9c3256d2e2d22dcb86966272ca21b4171030396c
John Moses <jmoses@phoenix-analytics.com>
2021-12-25 06:56:17 -0500

    add files

---
+++ profoliodown.R
@@ -0,0 +1,15 @@
+
+
+# https://www.business-science.io/code-tools/2021/12/20/portfoliodown.html
+
+
+
+#devtools::install_github("business-science/portfoliodown", force = TRUE)
+#blogdown::install_hugo("0.91.0")
+library(portfoliodown)
+
+
+new_portfolio_site()
+
+serve_site()
+
---
+++ themes/raditian-free-hugo-theme-data-science/README.md
@@ -0,0 +1,5 @@
+# MODIFIED VERSION OF RADITIAN HUGO THEME
+
+- __Designed for Data Scientists__ that wish to create and showcase project portfolios
+- __Integrates with Blogdown__, an R package for generating websites
+
